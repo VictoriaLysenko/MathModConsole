c2fe07e9d181a196a22df39a565fb54cfc3bb15b
Viktoria <viktorialysenko83@gmail.com>
2017-11-21 22:42:54 +0200

    Add method implementation

--- MathModCosole/Methods/EulerMethod.cs
+++ MathModCosole/Methods/EulerMethod.cs
@@ -6,7 +6,60 @@ using System.Threading.Tasks;
 
 namespace MathModCosole.Methods
 {
-    public class EulerMethod
+    public class EulerMethod : IMethod
     {
+        private double _t0;
+        public double t0
+        {
+            get => _t0;
+            set
+            {
+                _t0 = value;
+            }
+        }
+
+        private double _T;
+        public double T
+        {
+            get => _T;
+            set
+            {
+                _T = value;
+            }
+        }
+
+        public EulerMethod(double a, double b, double h)
+        {
+            t0 = a;
+            T = b;
+
+            func function = new func(Function);
+            Console.WriteLine("Result : {0}", EulerMethodRealisation(function, a, b, h));
+        }
+
+        public delegate double func(double t);
+
+        public double Function(double t)
+        {
+            return 6 * (t - 1);
+        }
+
+        public double StartValue(double t)
+        {
+            return t;
+        }
+
+        public static double EulerMethodRealisation(func f, double a, double b, double step)
+        {
+            double result = a;
+
+            for (double x = a; x <= b; x += step)
+            {
+                Console.WriteLine("\t" + x + "\t" + result);
+                result += step * f(result);
+            }
+
+            return result;
+        }
     }
 }
--- MathModCosole/Program.cs
+++ MathModCosole/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MathModCosole.Methods;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,9 @@ namespace MathModCosole
     {
         static void Main(string[] args)
         {
+            EulerMethod euler = new EulerMethod(1, 2, 0.5);
+
+            Console.ReadLine();
         }
     }
 }
